447ae3c226f669f411bad33ab4f39f5e30f8a58d
gushoubang <gushoubang@126.com>
2017-06-20 17:55:01 +0800

    添加上了路由控制

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,20 @@ import './App.css';
 
 import Button from 'antd/lib/button';
 
+import Home from './components/home'
+import Plan from './components/plan'
+import TestRouter from './components/testrouter.js'
+
+// 引入路由
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
+const history = createHistory()
+
+
 class App extends Component {
   state = {users: []};
   componentDidMount() {
@@ -19,7 +33,22 @@ class App extends Component {
         {this.state.users.map(user =>
           <div key={user.id}>{user.username}</div>
         )}
+
         <Button type="primary">Button</Button>
+        <Router history = {history}>
+            <div className="contentBox">
+              <ul className="nav">
+                <li><Link to="/">首页</Link></li>
+                <li><Link to="/plan">计划表</Link></li>
+                <li><Link to="/test">二级路由</Link></li>
+              </ul>
+              <div className="content"> 
+                <Route exact path="/" component={Home}/>
+                <Route path="/plan" component={Plan}/>
+                <Route path="/test" component={TestRouter}/>
+              </div>
+          </div>
+        </Router>
       </div>
     );
   }
